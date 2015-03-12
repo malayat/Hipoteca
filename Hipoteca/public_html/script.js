22b5d416099edf2955e90f9eb629ca38dd3489a7
maaa020812 <maaa020812@alejo-sri>
2015-03-12 17:02:10 -0500

    File script angular controller

---
+++ Hipoteca/public_html/script.js
@@ -0,0 +1,48 @@
+/* 
+ * Copyright (C) 2015 maaa020812
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var app = angular.module("app", []);
+
+app.controller("HipotecaController", ['$scope', function ($scope) {
+        $scope.hipoteca = {
+            idHipoteca: 3,
+            nombre: "Juan",
+            ape1: "García",
+            ape2: "Perez",
+            nif: "12345678Z",
+            edad: 32,
+            telefono: "964564563",
+            email: "juan_garcia_perez@gmail.com",
+            ingresosMensuales: 1200,
+            cuotaMensual: 492,
+            interesAplicado: 0.987,
+            datosFinancieros: {
+                capital: 150000,
+                tipoInteres: "VARIABLE",
+                tipoEuribor: "EURIBOR3",
+                euribor: 0.287,
+                diferencial: 0.9,
+                interesFijo: 0,
+                plazoAnyos: 30
+            },
+            productosContratados: {
+                seguroCasa: true,
+                nomina: true,
+                seguroVida: false
+            }
+        };
+    }]);
